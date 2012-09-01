86bd81a3e63e1245154ae91359aa968918357018
4apaev <4apaev@gmail.com>
2012-09-01 20:12:23 +0300

    nothing new...

--- client/src/profile.js
+++ client/src/profile.js
@@ -1,91 +1,91 @@
-var profiles = {}, config = {};
-    config.icon = "http://eu.media.blizzard.com/d3/icons/";
-    config.api  = "http://eu.battle.net/api/d3/";
-    config.ui =  {};
-        config.ui.inp =  $("#battleTag");
-        config.ui.btn =  $("#go");
-    config.bTag = "";
-    config.dummy = "Djz-2157";
+var profiles = {}, config;
+config = {
+    icon: "http://eu.media.blizzard.com/d3/icons/",
+    api: "http://eu.battle.net/api/d3/",
+    ui: { inp: "#battleTag", btn: "#go" },
+    dummy: "Jargon-2813",
+    init : function () { var b = $(config.ui.inp).val().replace("#", "-"), lnk = config.api + "profile/" + b + "/";
+        config.bTag = b;
+        parseProfile(lnk);
+    }
+};
 
-function init () {
-    var l = config.api+"profile/"+config.dummy+"/";
-    config.bTag = config.dummy;
-    parseProfile (l)
+$(document).ready(function () {
+    parseProfile(config.api+"profile/"+config.dummy+"/");
+    $(config.ui.btn).on("click", function () {
+        ($(config.ui.inp).val() === "")? alert("nono") : config.init();
+    });
+});
+
+function fetchData(url) {
+    return $.ajax({ url: url, type: "GET", dataType: "jsonp" });
+}
+function pTag(str) {
+    return str.replace("#", "-");
 }
 
-function getJson (lnk) {
-    return $.ajax({ type: "GET", dataType: "jsonp", url: lnk})
-}
-
-function parseProfile (lnk) {
-
-    profiles[config.bTag] = {};
-
-    getJson(lnk).then(function (dta) {
-
-        var last = dta.lastHeroPlayed;
-
-        for (var i in dta.heroes){
-
-            var mm = getJson(lnk+"hero/"+dta.heroes[i].id );
-
-            mm.then(function (data) {
-
-                data.gender = ( data.gender==0 ) ? "male" : "female";
-                data.last = (data.id == last) ? true : false;
-                data.items = parseItems (data.items);
-                //TODO in case of same name for all heroes in profile
-                profiles[config.bTag][data.name]=data ;
-
-                renderHero(data);
-            });
-
-        }
+function parseProfile(lnk) {
+    var mainUrl = lnk, heroUrl = mainUrl + "hero/", fData = fetchData(lnk);
+    fData.then(function (data) {
+        var bTag = pTag(data.battleTag), last = data.lastHeroPlayed;
+        profiles[bTag] = {};
+        newHeros(data, heroUrl, last, bTag);
     })
-
 }
 
-
-function renderHero (hero) {
-
-    var li = document.createElement("li");
-    li.innerHTML = "<h3>"+hero.name+"</h3><span>"+hero.level+"</span>";
-    document.getElementById("heroes").appendChild(li);
+function newHeros(data, heroUrl, last, bTag) {
+    for (var i in data.heroes){
+        var fHeroes = fetchData(heroUrl+data.heroes[i].id);
+        fHeroes.then(function(dta){
+            dta.gender = (dta.gender === 0 ) ? "male" : "female";
+            dta.last = (dta.id === last) ? true : false;
+            profiles[bTag][dta.name] = dta;
+            newItems(dta, bTag)
+        })
+    };
 }
 
-
-
-function  parseItems (items) {
-    var newItems = {};
-    for (var i in items){
-        (function(i) {
-            getJson(config.api+"data/"+items[i].tooltipParams).then(function (data) {
-                newItems[i] =  data;
+function newItems(dta, bTag) {
+    for(var i in dta.items){
+        var fItems = fetchData(config.api+"data/"+dta.items[i].tooltipParams);
+        (function (i) {
+            fItems.then(function(d){
+                profiles[bTag][dta.name].items[i] = d;
+                ko.applyBindings(new GearViewModel(profiles[bTag][dta.name]));
             })
         })(i);
     }
-    return newItems;
+}
+
+
+var GearViewModel = function(hero) {
+
+
+    this.bracers     =  ko.observable(hero.items.bracers     );
+    this.feet        =  ko.observable(hero.items.feet        );
+    this.hands       =  ko.observable(hero.items.hands       );
+    this.head        =  ko.observable(hero.items.head        );
+    this.leftFinger  =  ko.observable(hero.items.leftFinger  );
+    this.legs        =  ko.observable(hero.items.legs        );
+    this.mainHand    =  ko.observable(hero.items.mainHand    );
+    this.neck        =  ko.observable(hero.items.neck        );
+    this.offHand     =  ko.observable(hero.items.offHand     );
+    this.rightFinger =  ko.observable(hero.items.rightFinger );
+    this.shoulders   =  ko.observable(hero.items.shoulders   );
+    this.torso       =  ko.observable(hero.items.torso       );
+    this.waist       =  ko.observable(hero.items.waist       );
+
+
 };
 
 
 
-$(document).ready(function() {
+//ko.applyBindings(new GearViewModel(profiles["hero"+i]));
 
-    init();
 
-    config.ui.btn.on("click", function(){
 
-        if (config.ui.inp.val() == "") {
-            alert("nono")
-        } else {
-            var b = config.ui.inp.val().replace("#", "-");
-            var l = config.api+"profile/"+ b+"/";
-            config.bTag = b;
-            parseProfile (l)
 
-        }
-    })
-});
+
 
 
 
